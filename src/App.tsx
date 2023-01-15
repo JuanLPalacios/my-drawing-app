c287c792fa826dfe2835004271cc7c759fb51dc8
JuanLP <juan.luis.palacios.p@gmail.com>
2023-01-15 08:39:32 -0500

    Add Layer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Layer } from './components/Layer';
+const App = () => {
+    const width = 600, heidgh = 600;
+    const canvas: HTMLCanvasElement = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = heidgh;
+    let ctx = canvas.getContext('2d');
+    if (!ctx) throw new Error('CanvasRenderingContext2D could not be accessed');
+    return <Layer canvas={canvas} />
 }
 
 export default App;
